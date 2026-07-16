43fb9716422bb0b4aa9c53accbc244aa9f9b11a9
kailash suthar <kailashsuthar4488@gmail.com>
2026-07-16 09:10:20 +0530

    create delete api

--- Backend/src/routes/chat.route.js
+++ Backend/src/routes/chat.route.js
@@ -7,6 +7,6 @@ const chatRoute = Router();
 chatRoute.post("/message", identifyUser, sendMessageController);
 chatRoute.get("/", identifyUser, getChatsController);
 chatRoute.get("/:chatId/messages", identifyUser, getMessagesController);
-
+chatRoute.delete("/delete/:chatId", authUser, deleteChat);
 
 export default chatRoute;
