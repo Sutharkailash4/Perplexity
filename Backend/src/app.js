f38998c1d3b739540a8fb31f0798f0af02f3f1fe
kailash suthar <kailashsuthar4488@gmail.com>
2026-07-22 11:28:53 +0530

    cors fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,29 +1,43 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
 import authRoute from "./routes/authentication.route.js";
 import chatRoute from "./routes/chat.route.js";
-import morgan from "morgan";
 
 const app = express();
 
-// Middlewares
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true, methods : ["GET", "POST", "PUT", "DELETE"] }));
+
+app.use(
+  cors({
+    origin: "https://perplexity-1-fczu.onrender.com" || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(cookieParser());
 app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is Running",
+  });
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/chat", chatRoute);
 
-// Health Check
-
-app.get("/", (req, res) => {
-    res.json({
-        message : "Server is Running"
-    })
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
 });
 
 export default app;
