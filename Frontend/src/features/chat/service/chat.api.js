a8bbb1ec5a116c6af4ef9124900f923ebe51912c
kailash suthar <kailashsuthar4488@gmail.com>
2026-07-22 11:08:37 +0530

    deploy

--- Frontend/src/features/chat/service/chat.api.js
+++ Frontend/src/features/chat/service/chat.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "/api",
+    baseURL: "https://perplexity-9mb2.onrender.com/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
