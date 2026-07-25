1510dde838c478e9fc74a2626866c33eb5a0bf93
kailash suthar <kailashsuthar4488@gmail.com>
2026-07-25 10:36:38 +0530

    integration

--- Frontend/src/features/authentication/service/auth.api.js
+++ Frontend/src/features/authentication/service/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://perplexity-5.onrender.com/api" || "http://localhost:3000/api",
+    baseURL:  "http://localhost:3000/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
