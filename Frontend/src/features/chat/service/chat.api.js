b5756a811957afdf198a5efada153767a73e5973
kailash suthar <kailashsuthar4488@gmail.com>
2026-07-25 08:58:42 +0530

    interagtion

--- Frontend/src/features/chat/service/chat.api.js
+++ Frontend/src/features/chat/service/chat.api.js
@@ -1,57 +1,53 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.DEV
+  ? "http://localhost:3000/api"
+  : "https://perplexity-5.onrender.com/api";
+
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json"
-    }
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
-export const sendMessageApiCall = async ({message, chat: chatId}) => {
-    try {
-        
-        const response = await api.post("/chat/message", { message, chat: chatId });
-        return response.data;
-
-    } catch (error) {
-        console.error(error.response?.data?.message || error.message);
-        throw error;
-    }
-}
+export const sendMessageApiCall = async ({ message, chat: chatId }) => {
+  try {
+    const response = await api.post("/chat/message", { message, chat: chatId });
+    return response.data;
+  } catch (error) {
+    console.error(error.response?.data?.message || error.message);
+    throw error;
+  }
+};
 
 export const getChatApiCall = async () => {
-    try {
-
-        const response = await api.get("/chat")
-        return response.data;
-
-    }   catch (error) {
-        console.error(error.response?.data?.message || error.message);
-        throw error;
-    } 
-}
+  try {
+    const response = await api.get("/chat");
+    return response.data;
+  } catch (error) {
+    console.error(error.response?.data?.message || error.message);
+    throw error;
+  }
+};
 
 export const getMessageApiCall = async (chatId) => {
-    try {
-         
-        const resposne = await api.get(`/chat/${chatId}/messages`)
-        return resposne.data;
-        
-    } catch (error) {
-        console.error(error.response?.data?.message || error.message);
-        throw error;
-    }
-}
+  try {
+    const response = await api.get(`/chat/${chatId}/messages`);
+    return response.data;
+  } catch (error) {
+    console.error(error.response?.data?.message || error.message);
+    throw error;
+  }
+};
 
 export const deleteChatApiCall = async (chatId) => {
-    try {
-
-        const response = await api.delete(`/chat/delete/${chatId}`)
-        return response.data;
-
-    } catch (error) {
-        console.error(error.response?.data?.message || error.message);
-        throw error;
-    }
-}
+  try {
+    const response = await api.delete(`/chat/delete/${chatId}`);
+    return response.data;
+  } catch (error) {
+    console.error(error.response?.data?.message || error.message);
+    throw error;
+  }
+};
